3b0e3be05157edd66acf241035230bdb0a49819d
zene <zeneansari@gmail.com>
2024-11-15 21:55:20 +0530

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,6 +1,6 @@
-'use client';  
-
 // src/app/page.tsx
+
+'use client';  
 import React, { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
